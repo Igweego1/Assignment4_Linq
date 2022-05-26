7597573879c9aab10c6af7290c7ee5c797d5e32d
Obiora Igwilo <igwiloobiora@outlook.com>
2022-05-26 07:55:55 +0100

    Add project files.

---
+++ Assignment4_Linq/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_Linq
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Gender Gender { get; set; }
+        public double Salary { get; set; }
+
+
+        public static List<Employee> GetEmployee()
+        {
+            return new List<Employee>
+            {
+                new Employee { ID = 1, Name = "Amarachi", Age = 10, Gender = Gender.Female, Salary = 1000.00},
+                new Employee { ID = 2, Name = "Bimbo", Age = 15, Gender = Gender.Male, Salary = 1500.00},
+                new Employee { ID = 3, Name = "Charles", Age = 30, Gender = Gender.Male, Salary = 3000.00},
+                new Employee { ID = 4, Name = "Ebuka", Age = 28, Gender = Gender.Male, Salary = 2500.00},
+                new Employee { ID = 5, Name = "Shola", Age = 45, Gender = Gender.Male, Salary = 2000.00},
+                new Employee { ID = 6, Name = "Francis", Age = 35, Gender = Gender.Male, Salary = 4000.00},
+                new Employee { ID = 7, Name = "James", Age = 25, Gender = Gender.Male, Salary = 5000.00},
+                new Employee { ID = 8, Name = "Giftie", Age = 20, Gender = Gender.Female, Salary = 3500.00},
+                new Employee { ID = 9, Name = "Muoyo", Age = 40, Gender = Gender.Female, Salary = 6000.00},
+                new Employee { ID = 10, Name = "zikora", Age = 19, Gender = Gender.Female, Salary = 7000.00},
+
+            };
+        }
+    }
+
+   
+    public enum Gender
+    {
+        Male,
+        Female,
+    }
+}
---
+++ Assignment4_Linq/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Assignment4_Linq
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var employeelist = Employee.GetEmployee();
+
+
+            //Age Greater than 25
+            var age = employeelist.Where(x => x.Age > 25)
+                                  .OrderByDescending(x => x.Age);
+                                  
+           
+
+            foreach (var item in age)
+            {
+                Console.WriteLine($"Name: {item.Name}   Age: {item.Age}");
+            }
+
+            Console.WriteLine();
+
+            // Data for Female Candidates
+            var allFemale = employeelist.Where(x => x.Gender == Gender.Female);
+
+
+            foreach (var item in allFemale)
+            {
+                Console.WriteLine($"Name:{item.Name}   Gender:{item.Gender}");
+            }
+
+
+            Console.WriteLine();
+
+            // Data of Age 20, 30 or 35 
+            var allCandidates = employeelist.Where(x => x.Age == 20 || (x.Age == 30) || (x.Age == 35))
+                                               .OrderBy(x => x.Age);
+            foreach (var item in allCandidates)
+            {
+                Console.WriteLine($"Name: {item.Name}   Age: {item.Age}");
+            }
+
+            Console.WriteLine();
+
+            // ID between 1 to 8
+
+            var allID = employeelist.Where(x => x.ID <= 8 && x.Gender == Gender.Female);
+
+            foreach (var item in allID)
+            {
+                Console.WriteLine($"Name: {item.Name}    Gender: {item.Gender}     ID: {item.ID}");
+            }
+        }
+    }
+}
